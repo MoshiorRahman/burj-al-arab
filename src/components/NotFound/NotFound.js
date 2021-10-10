9a34a66a0a8e6645141fb464907527e6092fa5ab
Moshior <moshiorrahman279312@gmail.com>
2021-10-11 05:24:12 +0600

    my second commit

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>This is Not Found section in this </h2>
+        </div>
+    );
+};
+
+export default NotFound;
